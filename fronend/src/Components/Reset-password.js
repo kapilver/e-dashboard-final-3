515a5985e3132e291b44d97347630bfe0610ceb9
kapilver <128472629+kapilver@users.noreply.github.com>
2023-04-04 12:02:55 +0530

    Add files via upload

---
+++ fronend/src/Components/Reset-password.js
@@ -0,0 +1,86 @@
+// import { Collection } from "mongoose";
+import React, { useState } from "react";
+
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2';
+
+
+const ResetPassword = (props) => {
+
+
+    const [error, setError] = useState(false);
+
+    let location = useLocation();
+    const email = location.state.email;
+    console.log("email " + location.state.email);
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate =useNavigate();
+
+    const collectData = async () => {
+
+
+        
+
+        
+        if (!newPassword , !confirmPassword ) {
+            setError(true)
+            return false;
+        }
+
+        if (newPassword !== confirmPassword) {
+             alert("Passwords don't match");
+        }else{
+            let result = await fetch('http://localhost:5000/updatepassword', {
+                method: 'post',
+                body: JSON.stringify({ email, newPassword }),
+                headers: {
+                    'Content-type': 'Application/json',
+                }
+            })
+            result = await result.json();
+
+
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Password Update successfully',
+                showConfirmButton: false,
+                timer: 1000
+              })
+
+            navigate("/");
+            console.log(result);
+            
+        }
+    }
+
+    
+    
+return (
+
+
+    <div className='register'>
+        <h1>Reset Password</h1>
+        <label className="lebel_rest">New Passoword</label>
+        <input className='inputbox' type="text" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder='Enter your password' />
+
+        {error && !newPassword && <span className='invalid_title'> Enter New Password   </span>}
+        <label className="lebel_rest">Confirm Passoword</label>
+
+        <input className='inputbox' type="text" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Confirm Password' />
+
+        {error && !confirmPassword && <span className='invalid_title'> Enter confirm password  </span>}
+
+
+        <button className='appButton' onClick={collectData} type='button'>Rest Passoword</button>
+
+
+
+    </div>
+)
+
+
+}
+
+export default ResetPassword;
